f75f413a390968f35f161c2e9f6912a649449453
Nc Galih <encegalih@gmail.com>
2022-04-09 16:17:11 +0700

    fix

--- src/layout/header.js
+++ src/layout/header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="h-20 text-gray-700 flex flex-row items-center justify-between px-6">
             <h1 className="text-2xl">Nullspace Calculator</h1>
-            <a href='https:github.com/ncgalih/nullspace'>
+            <a href='https://github.com/ncgalih/nullspace'>
                 <Github width={26} height={26} />
             </a>
         </div>
